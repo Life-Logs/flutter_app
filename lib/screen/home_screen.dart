855717ccea2ba1ebf66d6ce278d4e502dd3659a8
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2023-10-23 23:27:28 +0900

    feat: add login button (home screen)

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:lifelog/screen/routine_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffebf6e1),
+      body: Column(
+        children: [
+          const SizedBox(height: 550),
+          Center(
+            child: Container(
+              width: 300,
+              height: 50,
+              decoration: BoxDecoration(
+                color: const Color(0xff64705B),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: ((context) => const RoutineScreen()),
+                      ));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff64705B),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                child: const Text(
+                  '로그인',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
