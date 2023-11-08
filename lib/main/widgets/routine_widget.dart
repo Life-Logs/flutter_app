c2ff7182f0f66bdb9ddbc27b2454966a584f8e34
leejungmin05 <leejungmin05@gmail.com>
2023-11-09 00:34:12 +0900

    feat: add routine card (fold/unfold)

--- lib/main/widgets/routine_widget.dart
+++ lib/main/widgets/routine_widget.dart
@@ -1,13 +1,79 @@
 import 'package:flutter/material.dart';
 
-class RoutineWidget extends StatelessWidget {
+class RoutineWidget extends StatefulWidget {
   const RoutineWidget({super.key});
 
   @override
+  State<RoutineWidget> createState() => _RoutineWidgetState();
+}
+
+class CardModel {
+  bool isClicked = false;
+}
+
+class _RoutineWidgetState extends State<RoutineWidget> {
+  List<CardModel> cards = List.generate(4, (index) => CardModel());
+
+  void onClick(int index) {
+    setState(() {
+      cards[index].isClicked = !cards[index].isClicked;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Text('루틴이다다다닫'),
+    return Scaffold(
+      backgroundColor: const Color(0xffebf6e1),
+      body: Column(
+        children: cards.asMap().entries.map((cardEntry) {
+          final index = cardEntry.key;
+          final card = cardEntry.value;
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: GestureDetector(
+              onTap: () => onClick(index),
+              child: Container(
+                height: card.isClicked ? 200 : 60,
+                decoration: BoxDecoration(
+                  color: const Color(0xfff5fdee),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                margin: const EdgeInsets.symmetric(vertical: 10),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: const Color(0xffebf6e1),
+  //     body: FutureBuilder(
+  //       builder: ((context, snapshot) {
+  //         if (snapshot.hasData) {
+  //           return const Column(
+  //             children: [
+  //               SizedBox(height: 30),
+  //               Expanded(child: makeRoutineList(snapshot)),
+  //             ],
+  //           );
+  //         }
+  //       }),
+  //     ),
+  //   );
+  // }
+
+  // ListView makeRoutineList(AsyncSnapshot<List<>> snapshot) {
+  //   return ListView.separated(
+  //     itemBuilder: (context, index) {
+  //       return null;
+
+  //   },
+  //   separatorBuilder: separatorBuilder,
+  //   itemCount: itemCount,)
+  // }
 }
